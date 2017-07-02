4b66457b476fadf33f1a3d7e14e4bbb37fd037ed
ylm123456 <2974107892@qq.com>
2017-07-02 21:02:54 +0800

    添加Appliccation,下载数据并显示

--- app/src/main/java/cn/ucai/jkbd/activity/ExamActivity.java
+++ app/src/main/java/cn/ucai/jkbd/activity/ExamActivity.java
@@ -3,17 +3,36 @@ package cn.ucai.jkbd.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
+import android.widget.TextView;
 import cn.ucai.jkbd.R;
-
+import cn.ucai.jkbd.ExamApplication;
+import cn.ucai.jkbd.bean.ExamInfo;
 /**
  * Created by clawpo on 2017/6/29.
  */
 
 public class ExamActivity extends AppCompatActivity {
+    TextView tvExamInfo;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        tvExamInfo = (TextView) findViewById(R.id.tv_examinfo);
+    }
+
+    private void initData() {
+        ExamInfo examInfo = ExamApplication.getInstance().getExamInfo();
+        if (examInfo!=null){
+            showData(examInfo);
+        }
+    }
+
+    private void showData(ExamInfo examInfo) {
+        tvExamInfo.setText(examInfo.toString());
     }
 }
