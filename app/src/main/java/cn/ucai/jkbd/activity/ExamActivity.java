64fd2d922880a551b441bbe5e36455b7d51ba206
ylm123456 <2974107892@qq.com>
2017-07-02 21:17:24 +0800

    下载考试题并显示

--- app/src/main/java/cn/ucai/jkbd/activity/ExamActivity.java
+++ app/src/main/java/cn/ucai/jkbd/activity/ExamActivity.java
@@ -3,16 +3,20 @@ package cn.ucai.jkbd.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
+import java.util.List;
 import cn.ucai.jkbd.R;
 import cn.ucai.jkbd.ExamApplication;
+import cn.ucai.jkbd.bean.Exam;
 import cn.ucai.jkbd.bean.ExamInfo;
 /**
  * Created by clawpo on 2017/6/29.
  */
 
 public class ExamActivity extends AppCompatActivity {
-    TextView tvExamInfo;
+    TextView tvExamInfo,tvExamTitle,tvOp1,tvOp2,tvOp3,tvOp4;
+    ImageView mImageView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +27,33 @@ public class ExamActivity extends AppCompatActivity {
 
     private void initView() {
         tvExamInfo = (TextView) findViewById(R.id.tv_examinfo);
+        tvExamTitle = (TextView) findViewById(R.id.tv_exam_title);
+        tvOp1 = (TextView) findViewById(R.id.tv_op1);
+        tvOp2 = (TextView) findViewById(R.id.tv_op2);
+        tvOp3 = (TextView) findViewById(R.id.tv_op3);
+        tvOp4 = (TextView) findViewById(R.id.tv_op4);
+        mImageView = (ImageView) findViewById(R.id.im_exam_image);
     }
 
     private void initData() {
         ExamInfo examInfo = ExamApplication.getInstance().getExamInfo();
         if (examInfo!=null){
             showData(examInfo);
+        }
+        List<Exam> examList = ExamApplication.getInstance().getExamList();
+        if (examList!=null){
+            showExam(examList);
+        }
+    }
+
+    private void showExam(List<Exam> examList) {
+        Exam exam = examList.get(0);
+        if (exam!=null){
+            tvExamTitle.setText(exam.getQuestion());
+            tvOp1.setText(exam.getItem1());
+            tvOp2.setText(exam.getItem2());
+            tvOp3.setText(exam.getItem3());
+            tvOp4.setText(exam.getItem4());
         }
     }
 
